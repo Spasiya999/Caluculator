735f2aca3ff791999ac8ccad2b592ce35f05ec5e
Sachintha Pasindu <spsd49@gmail.com>
2023-05-05 12:26:18 +0530

    Function add

--- assests/js/script.js
+++ assests/js/script.js
@@ -5,14 +5,44 @@ const display = document.querySelector('#display')
 for (btn of btns) {
   btn.addEventListener("click", function () {
     if(this.value == 'eq'){
-      let result = eval(display.value)
-      return display.value = result;
+      return evaluate()
     }
     if(this.value == 'clear'){
       return display.value = '';
     }
-    // console.log("ff");
+    if(this.value == '^'){
+      return display.value += '**';
+    }
+    if(this.value == '%'){
+      return display.value += '/100';
+    }
+    if(this.value == 'sqrt'){
+      return display.value = Math.sqrt(parseFloat(display.value));
+    }
     // console.log(this.value)
     display.value += this.value;
   });
 }
+
+// function evaluate(){
+//   if(display.value != ''){
+//     let result = eval(display.value)
+//     return display.value = result;
+//   }
+//   return false;
+// }
+
+//arrow function
+const evaluate = () =>{
+  if(display.value != ''){
+    let result = eval(display.value)
+    return display.value = result;
+    }
+    return false;
+}
+
+display.addEventListener('keypress', (e) =>{
+  if(e.key === 'Enter'){
+    return evaluate();
+  }
+});
